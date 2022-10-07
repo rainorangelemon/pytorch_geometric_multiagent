f9608f4a12ef2df5a870a71addbb7b70979fd640
rainorangelemon <rainorangelemon@gmail.com>
2022-10-07 18:22:21 -0400

    add change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,12 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "sphinx.ext.napoleon",
+    "myst_parser",
 ]
 
 html_theme = 'furo'
+html_title = "PyG-MultiAgent Documentation"
 html_logo = ('https://raw.githubusercontent.com/rainorangelemon/pygma_sphinx_theme/'
              'master/pygma_sphinx_theme/static/img/pygma_logo.png')
 html_favicon = ('https://raw.githubusercontent.com/rainorangelemon/pygma_sphinx_theme/'
@@ -50,6 +53,17 @@ html_static_path = ["_static"]
 html_css_files = [
     "css/custom.css",
 ]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# Napoleon settings
+napoleon_use_ivar = True
+napoleon_use_admonition_for_references = True
+# See https://github.com/sphinx-doc/sphinx/issues/9119
+napoleon_custom_sections = [("Returns", "params_style")]
 
 # # -- Options for HTML output
 
